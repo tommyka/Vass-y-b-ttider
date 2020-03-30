83e4cd56117b5c96f10abe478d5c0f2c104bf5c8
Tommy Kjær Andersen <dommy.ka@gmail.com>
2020-03-30 08:21:57 +0200

    Change to spaces for indentation

--- src/script/EventDispatcher.ts
+++ src/script/EventDispatcher.ts
@@ -2,83 +2,75 @@
  * @author mrdoob / http://mrdoob.com/
  */
 interface iEvent {
-	type:string;
-	target?:any;
-	data?:any
+  type: string;
+  target?: any;
+  data?: any;
 }
 
 class EventDispatcher {
-	private _listeners:any;
-	constructor(){
+  private _listeners: any;
+  constructor() {}
 
-	}
+  addEventListener(type: string, listener: Function) {
+    if (this._listeners === undefined) this._listeners = {};
 
-	addEventListener(type:string, listener:Function){
-		if ( this._listeners === undefined ) this._listeners = {};
+    var listeners = this._listeners;
+    if (listeners[type] === undefined) {
+      listeners[type] = [];
+    }
 
-		var listeners = this._listeners;
-		if ( listeners[ type ] === undefined ) {
-			listeners[ type ] = [];
-		}
+    if (listeners[type].indexOf(listener) === -1) {
+      listeners[type].push(listener);
+    }
+  }
 
-		if ( listeners[ type ].indexOf( listener ) === - 1 ) {
-			listeners[ type ].push( listener );
-		}
-	}
+  hasEventListener(type: string, listener: Function) {
+    if (this._listeners === undefined) return false;
 
-	hasEventListener(type:string, listener:Function ) {
+    var listeners = this._listeners;
+    if (
+      listeners[type] !== undefined &&
+      listeners[type].indexOf(listener) !== -1
+    ) {
+      return true;
+    }
 
-		if ( this._listeners === undefined ) return false;
+    return false;
+  }
 
-		var listeners = this._listeners;
-		if ( listeners[ type ] !== undefined && listeners[ type ].indexOf( listener ) !== - 1 ) {
-			return true;
-		}
+  removeEventListener(type: string, listener: Function) {
+    if (this._listeners === undefined) return;
 
-		return false;
-	}
+    var listeners = this._listeners;
+    var listenerArray = listeners[type];
 
-	removeEventListener( type:string, listener:Function ) {
-		if ( this._listeners === undefined ) return;
+    if (listenerArray !== undefined) {
+      var index = listenerArray.indexOf(listener);
+      if (index !== -1) {
+        listenerArray.splice(index, 1);
+      }
+    }
+  }
 
-		var listeners = this._listeners;
-		var listenerArray = listeners[ type ];
+  dispatchEvent(event: iEvent) {
+    if (this._listeners === undefined) return;
 
-		if ( listenerArray !== undefined ) {
-			var index = listenerArray.indexOf( listener );
-			if ( index !== - 1 ) {
-				listenerArray.splice( index, 1 );
-			}
-		}
-	}
+    var listeners = this._listeners;
+    var listenerArray = listeners[event.type];
 
-	dispatchEvent( event:iEvent ) {
+    if (listenerArray !== undefined) {
+      event.target = this;
 
-		if ( this._listeners === undefined ) return;
+      var array: any = [];
+      var length = listenerArray.length;
 
-		var listeners = this._listeners;
-		var listenerArray = listeners[ event.type ];
+      for (var i = 0; i < length; i++) {
+        array[i] = listenerArray[i];
+      }
 
-		if ( listenerArray !== undefined ) {
-
-			event.target = this;
-
-			var array:any = [];
-			var length = listenerArray.length;
-
-			for ( var i = 0; i < length; i ++ ) {
-
-				array[ i ] = listenerArray[ i ];
-
-			}
-
-			for ( var i = 0; i < length; i ++ ) {
-
-				array[ i ].call( this, event );
-
-			}
-
-		}
-
-	}
+      for (var i = 0; i < length; i++) {
+        array[i].call(this, event);
+      }
+    }
+  }
 }
--- src/script/model.ts
+++ src/script/model.ts
@@ -1,80 +1,80 @@
 /// <reference path="EventDispatcher.ts" />
 class Model extends EventDispatcher {
+  VASSOY: string = "v-s";
+  STAVANGER: string = "s-v";
 
-	VASSOY:string = "v-s";
-	STAVANGER:string = "s-v";
+  //Data
 
-	//Data
+  data: any = [];
+  route: any = {};
+  red: any = [];
 
-	data:any = [];
-	route: any = {};
-	red: any = [];
+  constructor() {
+    super();
+  }
 
-	constructor(){
-		super();
-	}
+  getRouteName(way: string): string {
+    if (way == this.VASSOY) {
+      return "Vassøy -> Stavanger";
+    } else if (way == this.STAVANGER) {
+      return "Stavanger -> Vassøy";
+    }
+  }
 
-	getRouteName(way:string):string{
-		if(way == this.VASSOY){
-			return "Vassøy -> Stavanger";
-		}else if (way == this.STAVANGER){
-			return "Stavanger -> Vassøy";
-		}
-	}
+  parseData(data: any) {
+    console.log("parseData");
 
-	parseData(data:any){
-		console.log("parseData");
+    var route = data.route;
+    for (var routename in route) {
+      for (var groupname in route[routename]) {
+        for (var i = 0; i < route[routename][groupname].length; i++) {
+          var t = route[routename][groupname][i];
+          t.time = parseInt(t.time.replace(":", ""));
+        }
+      }
+    }
 
-		var route  = data.route;
-		for(var routename in route){
-			for(var groupname in route[routename]){
-				for(var i = 0; i < route[routename][groupname].length; i++){
+    this.route = route;
 
-					var t = route[routename][groupname][i];
-					t.time = parseInt(t.time.replace(":", ""));
-				}
-			}
-		}
+    var red = data.red;
+    for (i = 0; i < red.length; i++) {
+      var routechange = red[i];
+      var daysplit: string[] = routechange.day.split("/");
+      routechange.day = new Date(
+        Number(daysplit[2]),
+        Number(daysplit[1]) - 1,
+        Number(daysplit[0])
+      );
+    }
 
-		this.route = route;
+    this.red = red;
+    return data;
+  }
 
-		var red = data.red;
-		for (i = 0; i < red.length; i ++){
-			var routechange = red[i];
-			var daysplit: string[] = routechange.day.split("/");
-			routechange.day = new Date(Number(daysplit[2]), Number(daysplit[1])-1, Number(daysplit[0]));
+  load(url: string = null) {
+    var self = this;
 
-		}
+    //if (window.navigator.onLine) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url || "boat.json", true);
+    req.onload = function(e) {
+      var data = self.parseData(JSON.parse(req.responseText));
+      self.data = data;
 
-		this.red = red;
-		return data;
-	}
+      localStorage.setItem("timesheet", req.responseText);
 
-	load(url:string = null){
-		var self = this;
+      self.dispatchEvent({ type: "complete", data: data });
+    };
+    req.onerror = function() {
+      var cacheData = localStorage.getItem("timesheet");
+      if (cacheData != null) {
+        self.data = self.parseData(JSON.parse(cacheData));
+        self.dispatchEvent({ type: "complete", data: self.data });
+      }
+    };
 
-		//if (window.navigator.onLine) {
-			var req = new XMLHttpRequest();
-			req.open("GET", url || "boat.json", true);
-			req.onload = function(e) {
-				var data = self.parseData(JSON.parse(req.responseText));
-				self.data = data;
-
-				localStorage.setItem("timesheet", req.responseText);
-
-				self.dispatchEvent({ type: "complete", data: data });
-			}
-			req.onerror = function() {
-				var cacheData = localStorage.getItem("timesheet");
-				if (cacheData != null) {
-
-					self.data = self.parseData(JSON.parse(cacheData));
-					self.dispatchEvent({ type: "complete", data: self.data });
-				}
-			}
-
-			req.send();
-		/*}else{
+    req.send();
+    /*}else{
 			var cacheData = localStorage.getItem("timesheet");
 
 			if (cacheData != null) {
@@ -83,101 +83,101 @@ class Model extends EventDispatcher {
 				self.dispatchEvent({ type: "complete", data: self.data });
 			}
 		}*/
-	}
+  }
 
-	dayGroupName(dayNumber:number){
-		if(dayNumber == 0){
-			return "Sunday";
-		}else if(dayNumber < 6){
-			return "Weekday";
-		}else if(dayNumber == 6){
-			return "Saturday";
-		}
-	}
-	groupDayNumber (groupName:string) {
-		if (groupName === 'Saturday') {
-			return 6;
-		} else if (groupName === 'Sunday') {
-			return 0;
-		}
+  dayGroupName(dayNumber: number) {
+    if (dayNumber == 0) {
+      return "Sunday";
+    } else if (dayNumber < 6) {
+      return "Weekday";
+    } else if (dayNumber == 6) {
+      return "Saturday";
+    }
+  }
+  groupDayNumber(groupName: string) {
+    if (groupName === "Saturday") {
+      return 6;
+    } else if (groupName === "Sunday") {
+      return 0;
+    }
 
-		var daynumber = new Date().getDay();
-		return daynumber > 1 && daynumber < 6 ? daynumber : 2;
-	}
+    var daynumber = new Date().getDay();
+    return daynumber > 1 && daynumber < 6 ? daynumber : 2;
+  }
 
+  getRedDay(date: Date): any {
+    console.log("getRedDay", this.red);
+    for (var i = 0; i < this.red.length; i++) {
+      var red = this.red[i];
+      if (date.toDateString() == red.day.toDateString()) {
+        return red;
+      }
+    }
+  }
 
-	getRedDay(date:Date):any{
-		console.log("getRedDay", this.red);
-		for (var i = 0; i < this.red.length; i++){
-			var red = this.red[i];
-			if(date.toDateString() == red.day.toDateString()){
-				return red;
-			}
-		}
-	}
+  getGroupFromDate(date: Date) {
+    var day = date || new Date();
 
-	getGroupFromDate(date:Date){
-		var day = date || new Date();
+    var group = this.dayGroupName(day.getDay());
 
+    return group;
+  }
 
-		var group = this.dayGroupName(day.getDay());
+  getTimeTables(way: string, group: string) {
+    var routes: any = [];
 
-		return group;
-	}
+    for (var key in this.route) {
+      if (key.indexOf(way) != -1) {
+        for (var routeday in this.route[key]) {
+          if (routeday.indexOf(group) != -1) {
+            routes.push(this.route[key][routeday]);
+          }
+        }
+      }
+    }
 
-	getTimeTables(way:string, group:string){
-		var routes:any = [];
+    return routes;
+  }
 
-		for(var key in this.route){
-			if(key.indexOf(way) != -1){
-				for(var routeday in this.route[key]){
-					if(routeday.indexOf(group) != -1){
-						routes.push(this.route[key][routeday]);
-					}
-				}
-			}
-		}
+  getCombinedTimeTable(way: string, group: string) {
+    var tables = this.getTimeTables(way, group);
+    for (var i = 1; i < tables.length; i++) {
+      tables[0] = tables[0].concat(tables[i]);
+    }
 
-		return routes;
-	}
+    return tables[0].sort(this.SORT_TIME);
+  }
 
-	getCombinedTimeTable(way:string, group:string){
-		var tables = this.getTimeTables(way, group);
-		for(var i = 1; i < tables.length; i ++){
-			tables[0] = tables[0].concat(tables[i]);
-		}
+  getCombinedTimeTablePlusNext(way: string, group: string) {
+    var data = this.getCombinedTimeTable(way, group);
 
-		return tables[0].sort(this.SORT_TIME);
-	}
+    var nextDay = this.groupDayNumber(group);
+    nextDay = (nextDay + 1) % 7;
 
-	getCombinedTimeTablePlusNext (way:string, group:string) {
-		var data = this.getCombinedTimeTable(way, group);
+    var nextDayData = this.getCombinedTimeTable(
+      way,
+      this.dayGroupName(nextDay)
+    );
+    for (var i = 0; i < nextDayData.length; i++) {
+      if (nextDayData[i].time < 400) {
+        data.push(nextDayData[i]);
+      } else {
+        break;
+      }
+    }
 
-		var nextDay = this.groupDayNumber(group);
-		nextDay = (nextDay + 1) % 7;
+    return data;
+  }
 
-		var nextDayData = this.getCombinedTimeTable(way, this.dayGroupName(nextDay));
-		for (var i = 0; i < nextDayData.length; i++) {
-			if (nextDayData[i].time < 400) {
-				data.push(nextDayData[i]);
-			} else {
-				break;
-			}
-		}
-
-		return data;
-	}
-
-
-	SORT_TIME(a:any,b:any){
-		var an = Number(a.time);
-		var bn = Number(b.time);
-		if(an < bn){
-			return -1;
-		}else if(an > bn){
-			return 1;
-		}else{
-			return 0;
-		}
-	}
+  SORT_TIME(a: any, b: any) {
+    var an = Number(a.time);
+    var bn = Number(b.time);
+    if (an < bn) {
+      return -1;
+    } else if (an > bn) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
 }
