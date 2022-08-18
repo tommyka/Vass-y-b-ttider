1559efa8a908d111322f2e051dc184324a05c4f5
Tommy Kjær Andersen <dommy.ka@gmail.com>
2022-08-18 22:32:06 +0200

    fix import

--- src/data/addNightRoutes.ts
+++ src/data/addNightRoutes.ts
@@ -1,9 +1,9 @@
-import { TimeList, Route } from "../components/app";
 import { Day } from "../types/Day";
+import { TimeList, Route } from "./useTimesheet";
 
-const AddNightRoutes = (list:TimeList, route:Route, day:Day) => {
-  const nightItems = route[day].filter(item => parseInt(item.time.replace(':', ''), 10) < 400);
+const AddNightRoutes = (list: TimeList, route: Route, day: Day) => {
+  const nightItems = route[day].filter((item) => parseInt(item.time.replace(":", ""), 10) < 400);
   return [...list, ...nightItems];
-}
+};
 
 export default AddNightRoutes;
